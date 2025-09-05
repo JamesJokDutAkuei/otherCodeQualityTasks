10687aba1e890088bcb044b23994e9151e8800f6
JamesJokDutAkuei <j.akuei@alutudent.com>
2025-09-05 12:30:49 +0200

    calculate matrix sum

--- codeImprovements/exercise9.js
+++ codeImprovements/exercise9.js
@@ -1,7 +1,19 @@
-function calculateMatrixSum(matrix) {
-  var sum = 0;
-  for (var i = 0; i < matrix.length; i++)
-    for (var j = 0; j < matrix[i].length; j++)
-      sum += matrix[i][j];
-  return sum;
+// function calculateMatrixSum(matrix) {
+//   var sum = 0;
+//   for (var i = 0; i < matrix.length; i++)
+//     for (var j = 0; j < matrix[i].length; j++)
+//       sum += matrix[i][j];
+//   return sum;
+// }
+
+const calculateMatrixSum = (matrix) => {
+  return matrix
+  .flat().reduce((sum, value) => sum + value)
 }
+
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6]
+];
+
+console.log(calculateMatrixSum(matrix));
